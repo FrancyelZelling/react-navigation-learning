5b397753f38a0396c8cb8cc2c3ba6b2a5c1b5b23
FrancyelZelling <zellingf@gmail.com>
2021-07-10 08:17:05 -0300

    Finished Nesting Navigators Section

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,12 +1,37 @@
 import React from 'react'
 import { View, Text, Button} from 'react-native'
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 
 export default function Home({navigation}) {
+
+  function test1() {
+    return (
+      <View>
+        <Text>
+          Test 1
+        </Text>
+      </View>
+    )
+  }
+
+  function test2() {
+    return (
+      <View>
+        <Text>
+          Test 2
+        </Text>
+        <Button title= "Go to Orders" onPress={() => navigation.navigate("About")} />
+      </View>
+    )
+  }
+
+  const Tab = createBottomTabNavigator();
+
   return (
-    <View>
-      <Text>Home Screen</Text>
-      <Button title="Go to About" onPress={() => navigation.navigate("About")}/>
-    </View>
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={test1} />
+      <Tab.Screen name="Test 2" component={test2} />
+    </Tab.Navigator>
   )
 }
 
